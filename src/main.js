e0d82e2ceb02e31ec93ec5711dd8999ac7fcd2c3
DY_XiaoDong <xiaodong@indouyin.cn>
2023-01-11 20:01:02 +0800

    Update src/main.js

--- src/main.js
+++ src/main.js
@@ -6,6 +6,9 @@ import store from './store'
 import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css'
 
+// font
+import './mono.less'
+
 const app = createApp(App)
 
 app.use(router).use(store).use(Antd).mount('#app')
