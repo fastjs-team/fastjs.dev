924b9d44741e1eb36d102ba606da7730ff049a04
DY_XiaoDong <xiaodong@indouyin.cn>
2023-01-19 11:17:14 +0800

    Update lang

--- src/lang/setup.js
+++ src/lang/setup.js
@@ -1,45 +1,38 @@
-import {message} from "ant-design-vue";
+import cookie from 'js-cookie'
 
-export default (path, relative) => {
-    // setup lang
-    const language = localStorage.getItem("lang") || "en"
-    if (language === "en") {
-        localStorage.setItem("lang", "en")
+export default function get(path, defaultValue) {
+    let language = cookie.get("language")
+    const langList = require(`./config.json`).langList
+    if (!language || !langList.includes(language)) {
+        language = "en"
+        cookie.set("language", "en")
     }
-    // get path.json
-    let lang = require(`./${path}.json`)
-    // setup unsupport
-    let unsupport = () => {
-        console.log(require(`./config.json`)["en"]);
-        console.log(language);
-        return void message.info(require(`./config.json`)[language]?.unsupport)
+    let index
+    try {
+        index = require(`./${path}.json`)
+    } catch (e) {
+        index = require(`./${path}/index.json`)
     }
-    // return
-    return target => {
-        // add relative path
-        if (relative) {
-            target = `${relative}.${target}`
-        }
-        target = target.split(".");
-        let result = lang[language];
-        target.forEach((v) => {
-            if (!result || !result[v]) {
-                // console -> group
-                console.group(`${language} lang unsupport`);
-                // console -> group -> log
-                console.log(`Get: ${v}`);
-                console.log("Result", lang["en"][v]);
-                // end group
-                console.groupEnd();
-                // use en
-                result = lang["en"];
-                // unsupport
-                unsupport();
-                // clear unsupport
-                unsupport = () => void 0;
+    if (language !== "en") {
+        const defaultLangIndex = index.zh
+        index = index[language]
+        // 递归检查
+        const check = (obj) => {
+            // 比对默认语言和当前语言，如果当前语言没有翻译，就用默认语言
+            for (let key in obj) {
+                if (typeof obj[key] === "object") {
+                    check(obj[key])
+                } else {
+                    if (!obj[key]) {
+                        obj[key] = defaultLangIndex[key]
+                    }
+                }
             }
-            result = result[v];
+        }
+    } else index = index.en
+    if (defaultValue)
+        defaultValue.split(".").forEach((item) => {
+            index = index[item]
         })
-        return result;
-    }
+    return index
 }
