06166996336f67e2dcd1ab78195691b3672c31e9
dy-xiaodong2022 <xiaodong@indouyin.cn>
2023-11-27 12:00:05 +0800

    update(router): update router mode

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import {createRouter, createWebHashHistory} from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import Home from '../views/home/index.vue'
 import Sponsor from '../views/sponsor/index.vue'
 
@@ -28,7 +28,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes: routes
 })
 
