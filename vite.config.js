80ecff016379e5ea1b7e395fd652aa4bb69df256
dy-xiaodong2022 <xiaodong@indouyin.cn>
2023-12-16 14:59:03 +0800

    update(vite): update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,32 +2,32 @@ import vue from '@vitejs/plugin-vue'
 import mkcert from "vite-plugin-mkcert";
 
 export default {
-  server: {
-    https: true
-   },
-  "plugins": [
-    vue(),
-    mkcert()
-  ],
-  "base": "./",
-  build: {
-    rollupOptions: {
-      input: {
-        main: './index.html',
-        404: './404.html'
-      }
+    server: {
+        https: true
+    },
+    plugins: [
+        vue(),
+        mkcert()
+    ],
+    base: "/",
+    build: {
+        rollupOptions: {
+            input: {
+                main: './index.html',
+                404: './404.html'
+            }
+        }
+    },
+    css: {
+        preprocessorOptions: {
+            less: {
+                javascriptEnabled: true
+            }
+        }
+    },
+    resolve: {
+        alias: {
+            "@": __dirname + "/src"
+        }
     }
-  },
-  "css": {
-    "preprocessorOptions": {
-      "less": {
-        "javascriptEnabled": true
-      }
-    }
-  },
-  "resolve": {
-    "alias": {
-      "@": __dirname + "/src"
-    }
-  }
 }
