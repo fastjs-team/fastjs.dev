2f2e7502bf28b3e58aa09b19d295a0a2e0c4bcfa
dy-xiaodong2022 <xiaodong@indouyin.cn>
2024-04-18 15:17:21 +0800

    feat(topbar): add custom dropdown menu

---
+++ src/components/topbar/language.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="dropdown">
+    <span>
+      {{ title }}
+      <CaretDownFilled />
+    </span>
+    <div class="dropdown-menu">
+      <a class="link" v-for="item in items" :key="item" @click="item.do()">
+        {{ item.name }}
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+import { CaretDownFilled } from "@ant-design/icons-vue";
+import { dom } from "jsfast";
+
+export default {
+  name: 'language-dropdown',
+  data() {
+    return {
+    };
+  },
+  mounted() {
+    const menu = dom.select(".dropdown-menu");
+    const maxHeight = menu.get("offsetHeight");
+    dom.select('.dropdown')[0]
+      .addEvent("mouseenter", () => {
+        menu.setStyle("height", maxHeight + "px")
+      })
+      .addEvent("mouseleave", () => {
+        menu.setStyle("height", "0")
+      })
+  },
+  props: {
+    title: {
+      type: String,
+      default: 'Language'
+    },
+    items: {
+      type: Array,
+      default: () => []
+    }
+  },
+  components: {
+    CaretDownFilled
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.dropdown {
+  position: relative;
+  display: inline-block;
+
+  .dropdown-menu {
+    position: absolute;
+    overflow: hidden;
+    transition: height 0.2s;
+    border-left: 3px solid #d4d4d4;
+    top: 50px;
+    width: 100%;
+
+    .link {
+      display: block;
+      height: 30px;
+      line-height: 30px;
+      margin: 0px 10px;
+    }
+  }
+}
+</style>
