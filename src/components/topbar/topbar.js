0cddf6472d76af4eefce0d02a55b2f531c7341d4
dy-xiaodong2022 <xiaodong@indouyin.cn>
2024-04-19 09:32:45 +0800

    refactor: format topbar.js

--- src/components/topbar/topbar.js
+++ src/components/topbar/topbar.js
@@ -1,6 +1,5 @@
-// check lang
 import cookie from "js-cookie";
-import config from "@/lang/config.json"
+import config from "@/lang/config.json";
 
 const lang = cookie.get("language");
 let langList = config.langList;
@@ -13,39 +12,39 @@ switch (lang) {
 }
 
 export default {
-  "left": [
+  left: [
     {
-      "name": "home",
-      "link": "/",
-      canHide: true
+      name: "home",
+      link: "/",
+      canHide: true,
     },
     {
-      "name": "sponsor",
-      "link": "/sponsor"
+      name: "sponsor",
+      link: "/sponsor",
     },
     {
-      "name": "playground",
-      "link": "https://stackblitz.com/edit/fastjs-playground?file=src%2Fmain.ts",
-      "newPage": true
-    }
+      name: "playground",
+      link: "https://stackblitz.com/edit/fastjs-playground?file=src%2Fmain.ts",
+      newPage: true,
+    },
   ],
-  "right": [
+  right: [
     {
-      "name": "docs",
-      "link": `https://${unshift}docs.fastjs.dev/`,
-      "newPage": true
+      name: "docs",
+      link: `https://${unshift}docs.fastjs.dev/`,
+      newPage: true,
     },
     {
-      "name": "lang",
-      "sub": langList.map((item) => {
+      name: "lang",
+      sub: langList.map((item) => {
         return {
-          "name": item,
-          "do": () => {
+          name: item,
+          do: () => {
             cookie.set("language", item);
             location.reload();
-          }
-        }
-      })
-    }
-  ]
-}
+          },
+        };
+      }),
+    },
+  ],
+};
