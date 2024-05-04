74fbae87549d3a063440c5cc35549b0447427948
xiaodong2008 <admin@xiaodong.moe>
2024-05-04 22:09:02 +0800

    setup template

--- src/counter.ts
+++
@@ -1,8 +0,0 @@
-import type { FastjsDom } from "jsfast";
-
-export function setupCounter(element: FastjsDom) {
-  let count = 0;
-  element.text("Count: 0").addEvent("click", (el) => {
-    el.text(`Count: ${++count}`);
-  });
-}
--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,12 @@
-import "./style.css";
-import typescriptLogo from "./typescript.svg";
-import logo from "./logo.svg";
+import "./style.scss";
 import { FastjsDom, dom } from "jsfast";
-import { setupCounter } from "./counter";
-import { setupRequest } from "./request";
-import { setupDate } from "./date";
+import { setupTopbar } from "./components/topbar";
+import { setupRouter } from "./router";
 
-dom.select("#app")?.html(`
-  <div>
-    <a href="https://docs.fastjs.dev" target="_blank">
-      <img src="${logo}" class="logo" alt="Fastjs logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Glossary/TypeScript" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello, Fastjs.</h1>
-    <div class="card">
-    <p id="date"></p>
-      <button id="counter" type="button"></button>
-    </div>
-    <div class="card">
-      <button id="send" type="button">Send Request</button>
-      <p id="request-result"></p>
-    </div>
-    <p class="read-the-docs">
-      Click on the logos to learn more
-    </p>
-  </div>
-`);
+import Home from "./pages/home/";
+import NotFound from "./pages/404";
 
-setupDate(dom.select("#date") as FastjsDom);
-setupCounter(dom.select("#counter") as FastjsDom);
-setupRequest();
+const root = dom.select<FastjsDom>("#app");
+
+setupTopbar(root);
+setupRouter(root, [Home], NotFound);
